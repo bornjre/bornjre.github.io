fb7b7399a1b7ae132927b08c3c5a11380bd33ab8
bornjre <bornjre@gmail.com>
2023-06-10 12:29:00 +0545

    build files

---
+++ docs/_app/immutable/nodes/6.edb864fc.js
@@ -0,0 +1,29 @@
+import{S as Q,i as R,s as V,k as n,q as f,a as u,l as s,m as l,r as p,h as t,c as m,b as r,D as a,I as z}from"../chunks/index.0629df7f.js";const W=!0,Z=Object.freeze(Object.defineProperty({__proto__:null,prerender:W},Symbol.toStringTag,{value:"Module"}));function X(N){let c,I,g,d,L,E,v,A,T,o,b,S,P,w,j,D,k,H,q,_,G,x,y,O;return{c(){c=n("h1"),I=f("Totality"),g=u(),d=n("p"),L=f(`There is a concept in computer security called an attack vector, which refers
+  to specific points in a system from which an attacker can exploit
+  vulnerabilities or weaknesses. To explain this using an analogy of a house,
+  someone could enter the house from various places like the door, window, or
+  cat door. These are the attack vectors of the house. The concept of an attack
+  vector is a type of totality, where totality refers to the complete or
+  comprehensive nature of something, considering all aspects or elements.
+  Achieving totality in real life is much harder. In the example of a house, a
+  SEAL team could bust through the house by jumping down from a chopper. At this
+  point, you might be asking what the point of being pedantic like this is
+  because the concept of totality can be used as a thinking tool in different
+  fields.`),E=u(),v=n("p"),A=f("Let's imagine the components that affect a person's health:"),T=u(),o=n("ul"),b=n("li"),S=f("Environment"),P=u(),w=n("li"),j=f("Diet"),D=u(),k=n("li"),H=f("Genetics"),q=u(),_=n("li"),G=f("Habits"),x=u(),y=n("p"),O=f(`Then, a study reveals that there are bacteria living in your gut, and the type
+  of gut bacteria is associated with different diseases and a person's health.
+  It's like discovering a whole new attack vector that we didn't know about
+  before.`)},l(e){c=s(e,"H1",{});var i=l(c);I=p(i,"Totality"),i.forEach(t),g=m(e),d=s(e,"P",{});var C=l(d);L=p(C,`There is a concept in computer security called an attack vector, which refers
+  to specific points in a system from which an attacker can exploit
+  vulnerabilities or weaknesses. To explain this using an analogy of a house,
+  someone could enter the house from various places like the door, window, or
+  cat door. These are the attack vectors of the house. The concept of an attack
+  vector is a type of totality, where totality refers to the complete or
+  comprehensive nature of something, considering all aspects or elements.
+  Achieving totality in real life is much harder. In the example of a house, a
+  SEAL team could bust through the house by jumping down from a chopper. At this
+  point, you might be asking what the point of being pedantic like this is
+  because the concept of totality can be used as a thinking tool in different
+  fields.`),C.forEach(t),E=m(e),v=s(e,"P",{});var M=l(v);A=p(M,"Let's imagine the components that affect a person's health:"),M.forEach(t),T=m(e),o=s(e,"UL",{});var h=l(o);b=s(h,"LI",{});var U=l(b);S=p(U,"Environment"),U.forEach(t),P=m(h),w=s(h,"LI",{});var B=l(w);j=p(B,"Diet"),B.forEach(t),D=m(h),k=s(h,"LI",{});var F=l(k);H=p(F,"Genetics"),F.forEach(t),q=m(h),_=s(h,"LI",{});var J=l(_);G=p(J,"Habits"),J.forEach(t),h.forEach(t),x=m(e),y=s(e,"P",{});var K=l(y);O=p(K,`Then, a study reveals that there are bacteria living in your gut, and the type
+  of gut bacteria is associated with different diseases and a person's health.
+  It's like discovering a whole new attack vector that we didn't know about
+  before.`),K.forEach(t)},m(e,i){r(e,c,i),a(c,I),r(e,g,i),r(e,d,i),a(d,L),r(e,E,i),r(e,v,i),a(v,A),r(e,T,i),r(e,o,i),a(o,b),a(b,S),a(o,P),a(o,w),a(w,j),a(o,D),a(o,k),a(k,H),a(o,q),a(o,_),a(_,G),r(e,x,i),r(e,y,i),a(y,O)},p:z,i:z,o:z,d(e){e&&t(c),e&&t(g),e&&t(d),e&&t(E),e&&t(v),e&&t(T),e&&t(o),e&&t(x),e&&t(y)}}}class $ extends Q{constructor(c){super(),R(this,c,null,X,V,{})}}export{$ as component,Z as universal};
